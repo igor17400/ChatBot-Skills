186a8398452f86bd3ea2707bbb6638a425c0a83b
Igor Lima <igorlima1740@gmail.com>
2020-03-03 18:33:07 -0300

    QnA chatbot code

--- README.md
+++ README.md
@@ -11,4 +11,26 @@ Examples of questions:
 - How many people live in United States?
 etc...
 
+1) I have cloned microsoft repository repository about qna multiturn --> https://github.com/microsoft/BotBuilder-Samples/tree/master/samples/csharp_dotnetcore/70.qnamaker-multiturn-sample
 
+- As we can in the image below, if we write "countries" or something similar, the chatbot display an adaptive card showing other options.
+
+![Local Chatbot](Images/chatbot-image1.png)
+
+- Then we can click in another option to continue the chat. 
+
+![Local Chatbot](Images/chatbot-image2.png)
+
+<br></br>
+
+![Local Chatbot](Images/chatbot-image3.png)
+
+- Another cool function of this chatbot is capability to recognize written language and not only buttons. Therefore I can type the name of a country in the middle of the conversation and the chatbot will give me the information about this country.
+
+![Local Chatbot](Images/chatbot-image4.png)
+
+As we can see this is a demostration 
+of a multiturn chat with information about countries. 
+
+
+## Linking the Countries FAQ with a skill in a main chatbot
---
+++ countriesQnA/Dialog/RootDialog.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder.AI.QnA.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+
+//az group deployment create --name "QnA-countries-chatbot" --resource-group "JarvisSkill" --template-file "template-with-preexisting-rg.json" --parameters appId = "aae25dc1-2aa0-4af2-b726-67e70e995156" appSecret="AtLeastSixteenCharacters_0" botId="countriesQnAAppservice" newWebAppName="countriesQnAWebApp" newAppServicePlanName="countriesServicePlan" appServicePlanLocation="westus" 
+
+namespace Microsoft.BotBuilderSamples.Dialog
+{
+    /// <summary>
+    /// This is an example root dialog. Replace this with your applications.
+    /// </summary>
+    public class RootDialog : ComponentDialog
+    {
+        /// <summary>
+        /// QnA Maker initial dialog
+        /// </summary>
+        private const string InitialDialog = "initial-dialog";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RootDialog"/> class.
+        /// </summary>
+        /// <param name="services">Bot Services.</param>
+        public RootDialog(IQnAMakerConfiguration services)
+            : base("root")
+        {
+            AddDialog(new QnAMakerMultiturnDialog(services));
+
+            AddDialog(new WaterfallDialog(InitialDialog)
+               .AddStep(InitialStepAsync));
+
+            // The initial child Dialog to run.
+            InitialDialogId = InitialDialog;
+        }
+
+        private async Task<DialogTurnResult> InitialStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // Set values for generate answer options.
+            var qnamakerOptions = new QnAMakerOptions
+            {
+                ScoreThreshold = QnAMakerMultiturnDialog.DefaultThreshold,
+                Top = QnAMakerMultiturnDialog.DefaultTopN,
+                Context = new QnARequestContext()
+            };
+
+            var noAnswer = (Activity)Activity.CreateMessageActivity();
+            noAnswer.Text = QnAMakerMultiturnDialog.DefaultNoAnswer;
+
+            var cardNoMatchResponse = new Activity(QnAMakerMultiturnDialog.DefaultCardNoMatchResponse);
+
+            // Set values for dialog responses.	
+            var qnaDialogResponseOptions = new QnADialogResponseOptions
+            {
+                NoAnswer = noAnswer,
+                ActiveLearningCardTitle = QnAMakerMultiturnDialog.DefaultCardTitle,
+                CardNoMatchText = QnAMakerMultiturnDialog.DefaultCardNoMatchText,
+                CardNoMatchResponse = cardNoMatchResponse
+            };
+
+            var dialogOptions = new Dictionary<string, object>
+            {
+                [QnAMakerMultiturnDialog.QnAOptions] = qnamakerOptions,
+                [QnAMakerMultiturnDialog.QnADialogResponseOptions] = qnaDialogResponseOptions
+            };
+
+            return await stepContext.BeginDialogAsync(nameof(QnAMakerMultiturnDialog), dialogOptions, cancellationToken);
+        }
+    }
+}
